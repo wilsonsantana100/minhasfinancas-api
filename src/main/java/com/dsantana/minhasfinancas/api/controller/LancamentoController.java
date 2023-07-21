6b98203b989043bce36eff9005a2b24b27b31af9
unknown <wilsonsantana100>
2023-07-20 21:52:29 -0300

    api 23/07/23

--- src/main/java/com/dsantana/minhasfinancas/api/controller/LancamentoController.java
+++ src/main/java/com/dsantana/minhasfinancas/api/controller/LancamentoController.java
@@ -65,6 +65,17 @@ public class LancamentoController {
 		return ResponseEntity.ok(lancamentos);
 		
 	}
+<<<<<<< HEAD
+=======
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@GetMapping("{id}")
+	public ResponseEntity obterLancamento( @PathVariable("id") Long id ) {
+		return service.obterPorId(id)
+					.map( lancamento -> new ResponseEntity(converter(lancamento), HttpStatus.OK) )
+					.orElseGet( () -> new ResponseEntity(HttpStatus.NOT_FOUND) );
+	}
+>>>>>>> 1a9c095 (commit api 20/07/23)
 
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
@@ -135,6 +146,24 @@ public class LancamentoController {
 	}
 	
 	
+<<<<<<< HEAD
+=======
+	private LancamentoDTO converter(Lancamento lancamento) {
+		return LancamentoDTO.builder()
+					.id(lancamento.getId())
+					.descricao(lancamento.getDescricao())
+					.valor(lancamento.getValor())
+					.mes(lancamento.getMes())
+					.ano(lancamento.getAno())
+					.status(lancamento.getStatus().name())
+					.tipo(lancamento.getTipo().name())
+					.usuario(lancamento.getUsuario().getId())
+					.build();
+					
+	}
+	
+	
+>>>>>>> 1a9c095 (commit api 20/07/23)
 	public Lancamento converter(LancamentoDTO dto) {
 		
 		Lancamento lancamento = new Lancamento();
